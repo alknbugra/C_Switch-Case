20af29be34ab85b8838a9cbb1894aefffe589ae2
Bugra Alkin <alknbugra@gmail.com>
2019-05-26 21:57:16 +0300

    Add files via upload

---
+++ ders18.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+void main(){
+	int gun;
+	printf("Haftanin kacinci gunundeyiz ?\n");
+	scanf("%d",&gun);
+	
+	switch(gun){
+		case 1:
+			printf("Bugun pazartesi (today is monday) ");
+			break;
+		case 2:
+			printf("Bugun sali (today is tuesday) ");
+			break;
+		case 3:
+			printf("Bugun carsamba (today is wednesday) ");
+			break;
+		case 4:
+			printf("Bugun persembe (today is thursday) ");
+			break;
+		case 5:
+			printf("Bugun cuma (today is friday) ");
+			break;
+		case 6:
+			printf("Bugun cumartesi (today is saturday) ");
+			break;
+		case 7:
+			printf("Bugun pazar (today is sunday) ");
+			break;
+	}
+}
